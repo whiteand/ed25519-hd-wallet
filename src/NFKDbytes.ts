fc86db6392039c5003978004bd5c7d08cca45d8b
whiteand <andrewbeletskiy@gmail.com>
2022-05-22 22:42:28 +0300

    rewrote without dependency on TextDecodre

--- src/NFKDbytes.ts
+++ src/NFKDbytes.ts
@@ -1,6 +1,16 @@
-import { TextEncoder } from 'util';
-
 export function NFKDbytes(str: string): Uint8Array {
-  const textEncoder = new TextEncoder();
-  return textEncoder.encode(str.normalize('NFKD'));
+  str = str.normalize('NFKD');
+  const points: number[] = [];
+  let i = 0;
+  while (i < str.length) {
+    const point = str.codePointAt(i);
+    if (point == null) {
+      i += 1;
+      continue;
+    }
+    points.push(point);
+    const ch = String.fromCodePoint(point);
+    i += ch.length;
+  }
+  return new Uint8Array(points);
 }
