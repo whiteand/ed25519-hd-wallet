28a82d0ca3f8684bbfaea4acc66e38c675e18174
whiteand <andrewbeletskiy@gmail.com>
2022-06-01 15:17:53 +0300

    added return type

--- src/deriveMnemonicPublic.ts
+++ src/deriveMnemonicPublic.ts
@@ -1,12 +1,13 @@
 import { deriveSeedPublicFromStringPath } from './deriveSeedPublicFromStringPath';
 import { mnemonicToSeed } from './mnemonicToSeed';
+import { IPublicDerivationNode } from './types';
 
 export async function deriveMnemonicPublic(
   path: string,
   mnemonic: string,
   passphrase: string = '',
   prefix: string = 'mnemonic'
-) {
+): Promise<IPublicDerivationNode | null> {
   const seed = await mnemonicToSeed(mnemonic, passphrase, prefix);
   return deriveSeedPublicFromStringPath(path, seed);
 }
